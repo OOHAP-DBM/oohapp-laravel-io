baf986861465b3612f215536e4b01b0944934e31
DbmRitik <dbm.laraveldeveloper7@gmail.com>
2024-11-28 11:19:43 +0530

    Code merge issue

--- storage/framework/views/3a3034a943af8c1666bbcf06b54e2e12e17358e7.php
+++
@@ -1,1 +0,0 @@
-<?php echo $__env->make('vendor.pagination.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH G:\xampp\htdocs\oohapp-laravel-io\resources\views/vendor/pagination/tailwind.blade.php ENDPATH**/ ?>
